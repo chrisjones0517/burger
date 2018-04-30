5f20fac3619b2d3f6d9108b7c461b5f8b158901e
Chris Jones <chrisjones0517@gmail.com>
2018-04-29 19:46:13 -0500

    added new database for deployment

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: 'burgers_db'
+    database: process.env.DB
 });
 
 connection.connect(function (err) {
